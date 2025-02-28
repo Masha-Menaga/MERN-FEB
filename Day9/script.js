a66fcb9128a888e19a7dbcc9debd81a1c2a9cf08
Masha-Menaga <rameast.mass@gmail.com>
2025-02-28 17:42:11 +0530

    Day 9 - JS

---
+++ Day9/script.js
@@ -0,0 +1,76 @@
+/*
+console.log("Vignesh!")
+a = 5;
+b = 10;
+
+c = a + b;
+
+// Eg:
+let x;
+x = 5;
+if (true) {
+    let x = 10;
+    console.log(x)
+}
+console.log(x)
+
+// Eg:
+const pi = 3.14;
+console.log(pi);
+//pi = 3.15; // 
+
+let u = 10;
+let y = "hai";
+let z = 10.5;
+
+console.log(u)
+console.log(y)
+console.log(typeof (u));
+console.log(typeof (y));
+
+console.log(typeof (z));
+
+let h;
+console.log(h)
+
+let age = 30;
+let isStudent = true;
+let name = "Masha";
+let value = null;
+let person = {
+    name: "menaga", age: 30
+}
+
+console.log(age)
+console.log(isStudent)
+console.log(name)
+console.log(value)
+console.log(person)
+console.log(typeof (age))
+console.log(typeof (isStudent))
+console.log(typeof (name))
+console.log(typeof (person))
+
+console.log("Age type is " + typeof (age))
+console.log("Age is " + age)
+
+*/
+
+//Arithmetic Operators
+let a = 10;
+let b = 5;
+console.log(a)
+console.log(b)
+console.log(a + b)
+console.log(a - b)
+console.log(a / b)
+console.log(a * b)
+console.log(a % b)
+console.log(a ** b)
+console.log(a++) // print a ->a++ - a+1 = 11
+// console.log(a)
+console.log(++a)
+
+console.log(a--) // print a ->a-- -a-1 = 11
+console.log(a)
+console.log(--a)
